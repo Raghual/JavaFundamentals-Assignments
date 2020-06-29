64c9cd8a2def50e33091a9173791cab9495e49cd
Raghual R <raviraghual@gmail.com>
2020-06-29 17:12:05 +0530

    Create CommandArgs2.java

---
+++ CommandArgs2.java
@@ -0,0 +1,10 @@
+package fundamnetalsJava;
+
+public class CommandArgs2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome "+args[0]);
+	}
+
+}
