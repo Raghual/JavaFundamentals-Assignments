857c13ef257f70ffb7c62c83fc7785b4e8d70ec7
Raghual R <raviraghual@gmail.com>
2020-06-29 17:11:15 +0530

    Create CommandArgs1.java

---
+++ CommandArgs1.java
@@ -0,0 +1,11 @@
+package fundamnetalsJava;
+
+public class CommandArgs1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0]+" Technologies "+args[1]);
+
+	}
+
+}
