8a9b4d00d56b2102d27ad83f1dc7206bf3a3af0a
Raghual R <raviraghual@gmail.com>
2020-06-29 17:16:10 +0530

    Create FlowControlPosOrNeg1.java

---
+++ FlowControlPosOrNeg1.java
@@ -0,0 +1,19 @@
+package fundamnetalsJava;
+
+import java.util.Scanner;
+
+public class FlowControlPosOrNeg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a>0) {
+			System.out.println("Positive");
+		}
+		else {
+			System.out.println("Negative");
+		}
+	}
+
+}
